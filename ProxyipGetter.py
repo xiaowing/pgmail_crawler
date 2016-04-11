ee1ce543ec90af719c08f4740de6e8af0718672c
xiaowing <wing0630@gmail.com>
2016-04-11 22:56:34 +0800

    modify the return value of getLatestProxys() and add the selection policy of proxy server.

--- ProxyipGetter.py
+++ ProxyipGetter.py
@@ -15,12 +15,21 @@ class proxy(object):
             if p <= 0 or p > 65535:
                 raise ValueError()
 
+        if speed[-1] == 's':
+            speed_sec = int(speed[:-1])
+        else:
+            speed_sec = int(speed)
+
         self.__ip_addr = ip_address
         self.__port_number = port
         self.__location = location
         self.__anonymous = anonymous
         self.__http_protocal = https.lower()
-        self.__speed = speed
+        self.__speed = speed_sec
+
+    @property
+    def speed(self):
+        return self.__speed
 
     def __str__(self):
         return str("%s://%s:%s" %(self.__http_protocal, self.__ip_addr, self.__port_number))
@@ -30,10 +39,6 @@ class proxy(object):
             return True
         else:
             return False
-
-    '''def pingBing(self):
-        r = requests.get("http://cn.bing.com/", proxies=str(self))
-        return r.status_code'''
 
     @classmethod
     def isValidIpFormat(cls, ip_str: str):
@@ -56,7 +61,7 @@ def getLatestProxys():
     content = r.text
     soup = BeautifulSoup(content,"html.parser")
     # Retrieve the h2 tag with the text as "最新代理"
-    h2_latest_tag = soup.find(h2_with_text_latest)    
+    h2_latest_tag = soup.find(__h2_with_text_latest)    
     sibling = h2_latest_tag.find_next_sibling("table",class_ = "list")
     table = sibling
     retList = []
@@ -76,27 +81,37 @@ def getLatestProxys():
         speed = str(ListTd[5].get_text()).strip()
         #time = str(ListTd[6].get_text()).strip()
         p = proxy(ipaddr, port, zone, anonymous, https, speed)
-        retList.append(p)
-    else:
-        for item in retList:
-            if item.isHttps() == True:
-                if not 'https' in proxies.keys():
-                    proxies['https'] = str(item)
+        if p.isHttps() == True:
+            if not 'https' in proxies.keys():
+                proxies['https'] = p
             else:
-                if not 'http' in proxies.keys():
-                    proxies['http'] = str(item)
+                if p.speed < proxies['https'].speed:
+                    proxies['https'] = p
         else:
-            return proxies
+            if not 'http' in proxies.keys():
+                proxies['http'] = p
+            else:
+                if p.speed < proxies['http'].speed:
+                    proxies['http'] = p
+    else:
+        result = { key:str(value) for key, value in proxies.items() }
+        return result
+
+def testUsability(proxies: dict):
+    if not isinstance(proxies, dict):
+        raise TypeError()
+
+    r = requests.Session().get("http://www.postgresql.org/", proxies=proxies)
+    return r.status_code
 
 
-def h2_with_text_latest(tag):
+def __h2_with_text_latest(tag):
         return tag.name == 'h2' and tag.get_text() == '最新代理'
 
 if __name__  == '__main__':
     proxies = getLatestProxys()
     print(proxies)
-    r = requests.Session().get("http://cn.bing.com/", proxies=proxies)
-    print(r.status_code)
+    print(testUsability(proxies))
 
 
 
